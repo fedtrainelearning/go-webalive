470d7b85e7dc2f2e4842bb09cd55e27fc3297a36
dblack <dblack@advancetafe.edu.au>
2014-07-31 16:07:23 +1000

    Remove urls from application and put into config
    Using flag to the get the path to the config file as a command line
    argument, and go-gypsy to parse the yaml config file

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 First attempt at a Go application:
  - Call urls, getting the status code and ensuring the sites don't go to sleep (to
    be called from a cron job)
+ - Using YAML for configuration (to store the urls)
+
+Config:
+
+    urls:
+        - http://someurl.com/example
+        - http://anotherurl.com/another
+
+Call with:
+
+    webalive --file=<path_to_yaml_config_file>
 
 Future plans:
  - Modify the application to concurrently call the urls
--- webalive.go
+++ webalive.go
@@ -1,19 +1,41 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/kylelemons/go-gypsy/yaml"
 	"net/http"
 )
 
-var urls = []string{
-	"http://smsprodweb1/eapplication",
-	"http://smsprodweb1/estudent",
-	"http://smsprodweb2/eapplication",
-	"http://smsprodweb2/estudent",
-}
+var file = flag.String("file", "config.yaml", "The path to the config file to use")
 
 func main() {
-	for _, url := range urls {
+	flag.Parse()
+
+	//get the root node in the config file
+	config, err := yaml.ReadFile(*file)
+	if err != nil {
+		fmt.Println("BOOM 1! ", err)
+		return
+	}
+
+	//get the count of urls
+	count, err := config.Count("urls")
+	if err != nil {
+		fmt.Println("Error getting urls from config: ", err)
+		return
+	}
+
+	fmt.Println("Number of urls found in config: ", count)
+
+	//iterate the urls
+	for x := 0; x < count; x++ {
+		url, err := config.Get(fmt.Sprintf("urls[%d]", x))
+		if err != nil {
+			fmt.Println("BOOM 3! ", err)
+			return
+		}
+
 		fmt.Println("Attempting url: ", url)
 
 		resp, err := http.Get(url)
